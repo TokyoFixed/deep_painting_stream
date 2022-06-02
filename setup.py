52e4ce6fa8c0fbc7abf9be09ba721a4e7146faa0
Bryan Battenfelder <bryanbatt@yahoo.com>
2022-06-02 14:46:06 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
-# from setuptools import find_packages
-# from setuptools import setup
+from setuptools import find_packages
+from setuptools import setup
 
-# with open('requirements.txt') as f:
-#     content = f.readlines()
-# requirements = [x.strip() for x in content if 'git+' not in x]
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
 
-# setup(name='deep_painting_stream',
-#       version="1.0",
-#       description="Project Description",
-#       packages=find_packages(),
-#       install_requires=requirements,
-#       test_suite='tests',
-#       # include_package_data: to install data from MANIFEST.in
-#       include_package_data=True,
-#       scripts=['scripts/deep_painting_stream-run'],
-#       zip_safe=False)
-mkdir -p ~/.streamlit/
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
+setup(name='deep_painting_stream',
+      version="1.0",
+      description="Project Description",
+      packages=find_packages(),
+      install_requires=requirements,
+      test_suite='tests',
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      scripts=['scripts/deep_painting_stream-run'],
+      zip_safe=False)
+# mkdir -p ~/.streamlit/
+# echo "\
+# [server]\n\
+# headless = true\n\
+# port = $PORT\n\
+# enableCORS = false\n\
+# \n\
+# " > ~/.streamlit/config.toml
